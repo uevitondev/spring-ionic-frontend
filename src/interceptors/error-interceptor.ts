b9940205c003ef7b07d25d81f10fcd072939774b
Ueviton Santos <uevitonsantos@outlook.com>
2023-01-19 18:49:12 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,6 +2,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS
 import { Injectable } from "@angular/core";
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 import { Observable } from "rxjs/Rx";
+import { FieldMessage } from "../models/fieldmessage.dto";
 import { StorageService } from "../services/storage.service";
 
 @Injectable()
@@ -35,6 +36,10 @@ export class ErrorInterceptor implements HttpInterceptor{
                 case 403:
                 this.handle403();
                 break;
+                
+                case 422:
+                this.handle422(errorObj);
+                break;
 
                 default:
                 this.handleDefaultError(errorObj);
@@ -42,7 +47,8 @@ export class ErrorInterceptor implements HttpInterceptor{
 
             return Observable.throw(errorObj);
         }) as any;
-    }   
+    }
+   
 
     handle403(){
         this.storage.setLocalUser(null);
@@ -52,6 +58,20 @@ export class ErrorInterceptor implements HttpInterceptor{
         let alert = this.alertCtrl.create({
             title: 'Erro 401: falha de autenticação',
             message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
             enableBackdropDismiss: false,
             buttons: [
                 {
@@ -74,6 +94,15 @@ export class ErrorInterceptor implements HttpInterceptor{
             ]
         });
         alert.present();        
+    }
+   
+
+    private listErrors(messages: FieldMessage[]): string {
+        let s: string = '';
+        for(var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 import { CidadeDTO } from '../../models/cidade.dto';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeService } from '../../services/domain/cidade.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 import { EstadoService } from '../../services/domain/estado.service';
 
 
@@ -22,12 +24,14 @@ export class SignupPage {
     public navParams: NavParams,
     public formBuilder: FormBuilder,
     public cidadeService: CidadeService,
-    public estadoService: EstadoService ) {
+    public estadoService: EstadoService,
+    public clienteService: ClienteService,
+    public alertCtrl: AlertController){
 
       this.formGroup = this.formBuilder.group({
         nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
         email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-        tipo : ['1', [Validators.required]],
+        tipoCliente : ['1', [Validators.required]],
         cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
         senha : ['123', [Validators.required]],
         logradouro : ['Rua Via', [Validators.required]],
@@ -64,7 +68,30 @@ export class SignupPage {
 
 
   signupUser(){
-    console.log("Enviou o formulario!")
+    console.log(this.formGroup.value);
+    this.clienteService.insert(this.formGroup.value)
+    .subscribe(response => {
+      this.showInsertOk();
+    },
+    error => {});    
   }
+
+  showInsertOk() {
+    let alert =  this.alertCtrl.create({
+      title: 'Sucesso',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text:'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+    
 
 }
