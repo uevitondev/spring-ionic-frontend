3f5cb1fe7e894dc543aa76478b5d9cad9c83bdfe
Ueviton Santos <uevitonsantos@outlook.com>
2023-02-01 11:38:39 -0300

    Carregando imagens dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../../config/api.config";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ProdutoService{
@@ -11,6 +12,11 @@ constructor(public http: HttpClient){
 
 findByCategoria(categoria_id: string){
     return this.http.get(`${API_CONFIG.baseUrl}/produtos/search?categorias=${categoria_id}`);
+}
+
+getSmallImageFromBucket(id: string) : Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+    return this.http.get(url, {responseType : 'blob'});
 }
 
 }
