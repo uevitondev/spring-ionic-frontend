c5508f1d05ce8295f9eef5255a5b5e1362721908
Ueviton Santos <uevitonsantos@outlook.com>
2023-02-01 13:37:51 -0300

    Criando página de carrinho de compras

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -13,7 +14,7 @@ export class ProdutoDetailPage {
 
   item: ProdutoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
+  constructor(public cartService: CartService, public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
@@ -31,6 +32,11 @@ export class ProdutoDetailPage {
     .subscribe(response => {
       this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
     }, error => {});
+  }
+
+  addToCart(produto: ProdutoDTO){
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
 import { LocalUser } from "../models/local_user";
+import { Cart } from "../models/cart";
 
 @Injectable()
 export class StorageService{
@@ -22,6 +23,23 @@ export class StorageService{
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
         }
 
+    }
+
+    getCart() : Cart{
+        let cart = localStorage.getItem(STORAGE_KEYS.cart);
+        if(cart == null){
+            return null;
+        }else{
+            return JSON.parse(cart);
+        }
+    }
+
+    setCart(cart : Cart){
+        if(cart == null){
+           localStorage.removeItem(STORAGE_KEYS.cart);
+        }else{
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(cart));
+        }
     }
 
 }
